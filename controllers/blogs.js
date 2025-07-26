780f0c47052a75902e73d978e2dabf9bdf2ea8d9
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-07-26 07:03:05 +0000

    converted .then functions to async/await functions

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,13 +1,12 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json((blogs))
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
   if (!(blog.likes)) {
@@ -17,9 +16,9 @@ blogsRouter.post('/', (request, response) => {
   if (!(blog.title) || !(blog.url)) {
     return response.status(400).end()
   }
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
+  
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
 
 module.exports = blogsRouter
