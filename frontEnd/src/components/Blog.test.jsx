2d79c9e7665154de54be2bb6cdc1d636ef059d7b
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-08-24 07:47:57 +0000

    added test for checking view button

--- frontEnd/src/components/Blog.test.jsx
+++ frontEnd/src/components/Blog.test.jsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
+import { useState } from 'react'
 
 test('renders content', async () => {
   const blog = {
@@ -15,4 +17,29 @@ test('renders content', async () => {
   expect(element).toBeDefined()
   expect(screen.queryByText('fso.com')).toBeNull()
   expect(screen.queryByText('likes')).toBeNull()
+})
+
+test('renders content', async () => {
+  const blog = {
+    title: "Checking rendering",
+    author: "FSO",
+    url: "fso.com",
+    likes: 0,
+    showFull: false
+  }
+
+  const VirtualBlogsEnv = () => {
+    const [blogs,setBlogs] = useState([blog])
+    return (<Blog blog={blogs[0]} blogs={blogs} setBlogs={setBlogs}/>)
+  }
+  const user = userEvent.setup()
+  render(<VirtualBlogsEnv/>)
+
+  expect(screen.queryByText('fso.com')).toBeNull()
+  expect(screen.queryByText('likes')).toBeNull()
+
+  await user.click(screen.getByText('View'))
+
+  expect(screen.getByText((content) => content.includes('fso.com'))).toBeDefined()
+  expect(screen.getByText((content) => content.includes('likes'))).toBeDefined()
 })
