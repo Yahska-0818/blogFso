97ad478e8b56ad20a424436a0646396f428565e3
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-08-26 14:28:54 +0000

    wrote test for writing a blog

--- frontEnd/tests/blog_app.spec.js
+++ frontEnd/tests/blog_app.spec.js
@@ -33,4 +33,20 @@ test.describe('Blog app', () => {
       await expect(page.getByText('Wrong credentials')).toBeVisible()
     })
   })
+  
+  test.describe('While logged in',()=> {
+    test('user can create a blog', async ({page}) => {
+      const loginTextboxes = await page.getByRole('textbox').all()
+      await loginTextboxes[0].fill('playwright')
+      await loginTextboxes[1].fill('test')
+      await page.getByRole('button', { name: 'login' }).click()
+      await page.getByRole('button', { name:'Create New Blog'}).click()
+      const blogTextBoxes = await page.getByRole('textbox').all()
+      await blogTextBoxes[0].fill('testing with playwright')
+      await blogTextBoxes[1].fill('sHayak')
+      await blogTextBoxes[2].fill('https://fullstackopen.com/en/part5/end_to_end_testing_playwright#exercises-5-17-5-23')
+      await page.getByRole('button', { name:'Save'}).click()
+      await expect(page.getByText('testing with playwright sHayak')).toBeVisible()
+    })
+  })
 })
